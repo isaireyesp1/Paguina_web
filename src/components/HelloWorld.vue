e684cd0e4091d3b6530ffacdaba60c5534044f71
isaireyesp1 <isaireyes2003p@gmail.com>
2025-03-19 08:07:34 -0600

    Modificando Archivo Vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,20 @@
-<!--<script setup>
-Propósito: Es la parte donde defines la lógica de tu componente utilizando la API de Composition de Vue 3.
-
-Funcionalidad: Aquí se definen variables reactivas, funciones, importaciones y lógica del componente. Esta sintaxis hace que el código sea más limpio y sencillo. -->
 <script setup>
 
 </script>
-<!-- <template>
-Propósito: Contiene la estructura HTML del componente, es decir, la interfaz de usuario (UI) que se renderiza.
-
-Funcionalidad: Dentro de este bloque defines cómo se verá tu componente, enlazando los datos y funciones con la interfaz visual. Usa directivas de Vue para trabajar con la reactividad y el comportamiento del componente.-->
 <template>
-
+<h1>INGENIERIA EN SISTEMAS</h1>
+<button class="btn">Ingresar</button>
 </template>
-<!--<style scoped>
-Propósito: Define los estilos CSS del componente.
-
-Funcionalidad: El atributo scoped asegura que los estilos definidos aquí solo se apliquen a este componente específico, sin afectar a otros componentes en la misma aplicación. -->
 <style scoped>
-
+h1{
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+font-size: 55px;
+color: rgb(70, 73, 70);
+}
+btn{
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+background-color: aquamarine;
+}
 </style>
 
 
